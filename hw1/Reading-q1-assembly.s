e9758469aec9a7f3d5eaccf1b538af0913af88e3
Eric Johnson <johnso94@stolaf.edu>
2020-02-10 16:09:13 +0000

    Upload New File

---
+++ hw1/Reading-q1-assembly.s
@@ -0,0 +1,197 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.globl	_main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Lcfi0:
+	.cfi_def_cfa_offset 16
+Lcfi1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Lcfi2:
+	.cfi_def_cfa_register %rbp
+	subq	$32, %rsp
+	movl	$0, -20(%rbp)
+	movl	$1, -24(%rbp)
+LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
+	cmpl	$10, -24(%rbp)
+	jg	LBB0_3
+## BB#2:                                ##   in Loop: Header=BB0_1 Depth=1
+	movq	__ZNSt3__14coutE@GOTPCREL(%rip), %rdi
+	movl	-24(%rbp), %esi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEElsEi
+	leaq	__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_(%rip), %rdi
+	movq	%rax, -8(%rbp)
+	movq	%rdi, -16(%rbp)
+	movq	-8(%rbp), %rdi
+	callq	*-16(%rbp)
+	movl	-24(%rbp), %esi
+	addl	$1, %esi
+	movl	%esi, -24(%rbp)
+	movq	%rax, -32(%rbp)         ## 8-byte Spill
+	jmp	LBB0_1
+LBB0_3:
+	xorl	%eax, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.private_extern	__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_
+	.globl	__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_
+	.weak_definition	__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_
+	.p2align	4, 0x90
+__ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_: ## @_ZNSt3__14endlIcNS_11char_traitsIcEEEERNS_13basic_ostreamIT_T0_EES7_
+Lfunc_begin0:
+	.cfi_startproc
+	.cfi_personality 155, ___gxx_personality_v0
+	.cfi_lsda 16, Lexception0
+## BB#0:
+	pushq	%rbp
+Lcfi3:
+	.cfi_def_cfa_offset 16
+Lcfi4:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Lcfi5:
+	.cfi_def_cfa_register %rbp
+	subq	$144, %rsp
+	movq	%rdi, -72(%rbp)
+	movq	-72(%rbp), %rdi
+	movq	(%rdi), %rax
+	movq	-24(%rax), %rax
+	movq	%rdi, %rcx
+	addq	%rax, %rcx
+	movq	%rcx, -32(%rbp)
+	movb	$10, -33(%rbp)
+	movq	-32(%rbp), %rsi
+	leaq	-48(%rbp), %rax
+	movq	%rdi, -80(%rbp)         ## 8-byte Spill
+	movq	%rax, %rdi
+	movq	%rax, -88(%rbp)         ## 8-byte Spill
+	callq	__ZNKSt3__18ios_base6getlocEv
+	movq	-88(%rbp), %rax         ## 8-byte Reload
+	movq	%rax, -24(%rbp)
+	movq	-24(%rbp), %rdi
+Ltmp0:
+	movq	__ZNSt3__15ctypeIcE2idE@GOTPCREL(%rip), %rsi
+	callq	__ZNKSt3__16locale9use_facetERNS0_2idE
+Ltmp1:
+	movq	%rax, -96(%rbp)         ## 8-byte Spill
+	jmp	LBB1_1
+LBB1_1:
+	movb	-33(%rbp), %al
+	movq	-96(%rbp), %rcx         ## 8-byte Reload
+	movq	%rcx, -8(%rbp)
+	movb	%al, -9(%rbp)
+	movq	-8(%rbp), %rdx
+	movq	(%rdx), %rsi
+	movq	56(%rsi), %rsi
+	movsbl	-9(%rbp), %edi
+Ltmp2:
+	movl	%edi, -100(%rbp)        ## 4-byte Spill
+	movq	%rdx, %rdi
+	movl	-100(%rbp), %r8d        ## 4-byte Reload
+	movq	%rsi, -112(%rbp)        ## 8-byte Spill
+	movl	%r8d, %esi
+	movq	-112(%rbp), %rdx        ## 8-byte Reload
+	callq	*%rdx
+Ltmp3:
+	movb	%al, -113(%rbp)         ## 1-byte Spill
+	jmp	LBB1_5
+LBB1_2:
+Ltmp4:
+	movl	%edx, %ecx
+	movq	%rax, -56(%rbp)
+	movl	%ecx, -60(%rbp)
+Ltmp5:
+	leaq	-48(%rbp), %rdi
+	callq	__ZNSt3__16localeD1Ev
+Ltmp6:
+	jmp	LBB1_3
+LBB1_3:
+	movq	-56(%rbp), %rdi
+	callq	__Unwind_Resume
+LBB1_4:
+Ltmp7:
+	movl	%edx, %ecx
+	movq	%rax, %rdi
+	movl	%ecx, -120(%rbp)        ## 4-byte Spill
+	callq	___clang_call_terminate
+LBB1_5:
+	leaq	-48(%rbp), %rdi
+	callq	__ZNSt3__16localeD1Ev
+	movq	-80(%rbp), %rdi         ## 8-byte Reload
+	movb	-113(%rbp), %al         ## 1-byte Reload
+	movsbl	%al, %esi
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEE3putEc
+	movq	-72(%rbp), %rdi
+	movq	%rax, -128(%rbp)        ## 8-byte Spill
+	callq	__ZNSt3__113basic_ostreamIcNS_11char_traitsIcEEE5flushEv
+	movq	-72(%rbp), %rdi
+	movq	%rax, -136(%rbp)        ## 8-byte Spill
+	movq	%rdi, %rax
+	addq	$144, %rsp
+	popq	%rbp
+	retq
+Lfunc_end0:
+	.cfi_endproc
+	.section	__TEXT,__gcc_except_tab
+	.p2align	2
+GCC_except_table1:
+Lexception0:
+	.byte	255                     ## @LPStart Encoding = omit
+	.byte	155                     ## @TType Encoding = indirect pcrel sdata4
+	.asciz	"\274"                  ## @TType base offset
+	.byte	3                       ## Call site Encoding = udata4
+	.byte	52                      ## Call site table length
+Lset0 = Lfunc_begin0-Lfunc_begin0       ## >> Call Site 1 <<
+	.long	Lset0
+Lset1 = Ltmp0-Lfunc_begin0              ##   Call between Lfunc_begin0 and Ltmp0
+	.long	Lset1
+	.long	0                       ##     has no landing pad
+	.byte	0                       ##   On action: cleanup
+Lset2 = Ltmp0-Lfunc_begin0              ## >> Call Site 2 <<
+	.long	Lset2
+Lset3 = Ltmp3-Ltmp0                     ##   Call between Ltmp0 and Ltmp3
+	.long	Lset3
+Lset4 = Ltmp4-Lfunc_begin0              ##     jumps to Ltmp4
+	.long	Lset4
+	.byte	0                       ##   On action: cleanup
+Lset5 = Ltmp5-Lfunc_begin0              ## >> Call Site 3 <<
+	.long	Lset5
+Lset6 = Ltmp6-Ltmp5                     ##   Call between Ltmp5 and Ltmp6
+	.long	Lset6
+Lset7 = Ltmp7-Lfunc_begin0              ##     jumps to Ltmp7
+	.long	Lset7
+	.byte	1                       ##   On action: 1
+Lset8 = Ltmp6-Lfunc_begin0              ## >> Call Site 4 <<
+	.long	Lset8
+Lset9 = Lfunc_end0-Ltmp6                ##   Call between Ltmp6 and Lfunc_end0
+	.long	Lset9
+	.long	0                       ##     has no landing pad
+	.byte	0                       ##   On action: cleanup
+	.byte	1                       ## >> Action Record 1 <<
+                                        ##   Catch TypeInfo 1
+	.byte	0                       ##   No further actions
+                                        ## >> Catch TypeInfos <<
+	.long	0                       ## TypeInfo 1
+	.p2align	2
+
+	.section	__TEXT,__text,regular,pure_instructions
+	.private_extern	___clang_call_terminate
+	.globl	___clang_call_terminate
+	.weak_definition	___clang_call_terminate
+	.p2align	4, 0x90
+___clang_call_terminate:                ## @__clang_call_terminate
+## BB#0:
+	pushq	%rax
+	callq	___cxa_begin_catch
+	movq	%rax, (%rsp)            ## 8-byte Spill
+	callq	__ZSt9terminatev
+
+
+.subsections_via_symbols
